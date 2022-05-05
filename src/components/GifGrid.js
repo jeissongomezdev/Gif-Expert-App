ec44c3ae0b4e447706b9d88727859d98fc7a9ea7
Jeisson Gómez <jeissongomez@hotmail.es>
2022-05-05 14:04:14 -0500

    Helpers - getGifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,28 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
 import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    getGifs();
-  }, []);
-  const getGifs = async () => {
-    const url =
-      "https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=ZBobojedShHw3a582C5c0etlxb2zudlO";
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
-
-    console.log(gifs);
-    setImages(gifs);
-  };
+    getGifs(category).then(setImages);
+  }, [category]);
 
   return (
     <>
